8d3560b4e2cbd4bc8420c1524bdd3be6d595fabb
Scream <76878170+BreettySun@users.noreply.github.com>
2022-04-24 00:18:14 +0800

    Add files via upload

---
+++ front-end/innovation/src/components/Backtop.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <el-backtop :visibility-height="200"></el-backtop>
+  </div>
+</template>
+<script>
+export default {
+  name: 'Backtop',
+}
+</script>
+<style scoped>
+</style>
