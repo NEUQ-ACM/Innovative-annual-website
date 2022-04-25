68578545a7eea7a2134510ff0f45f94a7aee82be
lyntele <87609128+lyntele@users.noreply.github.com>
2022-04-25 19:27:04 +0800

    完成了部分作品展示功能

--- front-end/innovation/src/views/Gallery.vue
+++ front-end/innovation/src/views/Gallery.vue
@@ -1,41 +1,54 @@
 <template>
   <el-container>
     <el-main class="main">
-      <div style="margin-top:90px;">
-        <a href="">
-          <span>学术论文</span>
-        </a>
+      <!-- 面包屑导航区域 -->
+      <el-breadcrumb separator="/" class="breadcrumb">
+        <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+        <el-breadcrumb-item>作品展示</el-breadcrumb-item>
+      </el-breadcrumb>
+      <div
+        style="margin-top: 90px"
+        class="top-content"
+        @click="toSearch('Academic')"
+      >
+        <a>学术论文</a>
         <i class="el-icon-arrow-right"></i>
       </div>
-      <div>
-        <a href="">
-          <span>改革项目成果</span>
-        </a>
+      <div @click="toSearch('Achievements')">
+        <a>改革项目成果</a>
         <i class="el-icon-arrow-right"></i>
       </div>
-      <div>
-        <a href="">
-          <span>创业推介项目</span>
-        </a>
+      <div @click="toSearch('Business')">
+        <a>创业推介项目</a>
         <i class="el-icon-arrow-right"></i>
       </div>
     </el-main>
   </el-container>
 </template>
 <script>
-import Header from '@/components/Header.vue'
-import Footer from '@/components/Footer.vue'
-import Backtop from '@/components/Backtop.vue'
 export default {
   name: "Gallery",
-  components: {
-    Header,
-    Footer,
-    Backtop
-  }
-}
+  methods: {
+    // 查询对应展示的作品
+    toSearch(type) {
+      this.$router.push({ path: `/itemlist`, query: { itemType: type } });
+      console.log(type)
+    },
+  },
+};
 </script>
 <style scoped>
+.el-breadcrumb {
+  justify-content: left !important;
+  /* margin: 0 !important; */
+  border-bottom: 1px solid #ccc !important;
+  border-top: 0 !important;
+  border-left: 0 !important;
+  border-right: 0 !important;
+}
+.top-content {
+  margin-top: 20px !important;
+}
 .el-header {
   height: 80px;
   padding: 0;
@@ -54,10 +67,11 @@ export default {
 .main div:hover {
   cursor: pointer;
 }
-.main div span {
+.main div a {
   line-height: 60px;
   margin-left: 20px;
   font-size: 18px;
+  text-decoration: none;
 }
 .main div i {
   margin-right: 20px;
---
+++ front-end/innovation/src/views/ItemList.vue
@@ -0,0 +1,132 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <!-- 面包屑导航区域 -->
+        <el-breadcrumb separator="/" class="breadcrumb">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item :to="{ path: '/gallery' }"
+            >作品展示</el-breadcrumb-item
+          >
+          <el-breadcrumb-item>{{
+            crumb[this.$route.query.itemType]
+          }}</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-divider></el-divider>
+      </el-col>
+      <!-- 条件筛选表单区 -->
+      <el-col :span="16" :offset="4">
+        <div class="formBox">
+          <el-form
+            ref="form"
+            :model="searchForm"
+            label-width="80px"
+            :inline="true"
+            label-position="left"
+          >
+            <el-form-item label="项目编号">
+              <el-input v-model="searchForm.a"></el-input>
+            </el-form-item>
+            <el-form-item label="项目名称">
+              <el-input v-model="searchForm.b"></el-input>
+            </el-form-item>
+            <el-form-item label="项目类型">
+              <el-select v-model="searchForm.c" placeholder="请选择">
+                <el-option label="创新训练项目" value="1"></el-option>
+                <el-option label="创业训练项目" value="2"></el-option>
+                <el-option label="创业实践项目" value="3"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="学校名称">
+              <el-input v-model="searchForm.d"></el-input>
+            </el-form-item>
+            <el-form-item label="学科门类">
+              <el-select v-model="searchForm.e" placeholder="请选择学科门类">
+                <el-option label="哲学" value="1"></el-option>
+                <el-option label="经济学" value="2"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="专业大类">
+              <el-select v-model="searchForm.f" placeholder="请选择专业大类">
+                <el-option label="区域一" value="shanghai"></el-option>
+                <el-option label="区域二" value="beijing"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item label="指导教师">
+              <el-input v-model="searchForm.g"></el-input>
+            </el-form-item>
+            <el-form-item label="项目成员">
+              <el-input v-model="searchForm.h"></el-input>
+            </el-form-item>
+            <el-form-item label="年份">
+              <el-select v-model="searchForm.i" placeholder="请选择活动区域">
+                <el-option label="2022年" value="2022"></el-option>
+                <el-option label="2021年" value="2021"></el-option>
+              </el-select>
+            </el-form-item>
+            <el-form-item>
+              <el-button type="primary" @click="search">查询</el-button>
+              <el-button @click="backToHome">返回</el-button>
+            </el-form-item>
+          </el-form>
+        </div>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      // 筛选表单项
+      searchForm: {
+        a: "",
+        b: "",
+        c: "",
+        d: "",
+        e: "",
+        f: "",
+        g: "",
+        h: "",
+        i: "", //等后端确认接收参数后改名
+      },
+      // 面包屑导航显示内容
+      crumb: {
+        Academic: "学术论文",
+        Achievements: "改革项目成果",
+        Business: "创业推介项目",
+      },
+    };
+  },
+  methods: {
+    // 放弃查询，返回主页
+    backToHome() {
+      this.$router.push('/')
+    }
+  },
+  mounted() {},
+};
+</script>
+
+<style scoped>
+.bg-purple-dark {
+  background: #99a9bf;
+}
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+.el-breadcrumb {
+  margin-bottom: 25px;
+  margin-top: 15px;
+}
+.formBox {
+  background-color: #f0f0f0;
+}
+
+.el-form {
+  padding: 20px;
+}
+</style>
