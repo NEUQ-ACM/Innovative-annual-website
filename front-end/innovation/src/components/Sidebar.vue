fb7a7df579b58ea726934adb76751734a9737d7f
OREO-code <2319841923@qq.com>
2022-05-04 15:47:18 +0800

    上传后台基本结构

---
+++ front-end/innovation/src/components/Sidebar.vue
@@ -0,0 +1,56 @@
+<template>
+	<div style="padding-left: 0%;height: 100%;">
+			<h4 style="color: blue;font-weight: bold;" >后台管理</h4>
+		    <el-menu
+				router
+		      default-active="Rotation"
+		      class="el-menu-vertical-demo"
+		      @open="handleOpen"
+		      @close="handleClose">
+		      <el-menu-item index="Rotation">
+		        <i class="el-icon-menu"></i>
+		        <span slot="title">首页轮播图</span>
+		      </el-menu-item>
+			  <el-menu-item index="Timeline">
+			    <i class="el-icon-menu"></i>
+			    <span slot="title">年会日程</span>
+			  </el-menu-item>
+			  <el-menu-item index="Recommend">
+			    <i class="el-icon-menu"></i>
+			    <span slot="title">创业推荐项目</span>
+			  </el-menu-item>
+			  <el-menu-item index="Team">
+			    <i class="el-icon-menu"></i>
+			    <span slot="title">优秀团队</span>
+			  </el-menu-item>
+		      <el-menu-item index="Notice">
+		        <i class="el-icon-menu"></i>
+		        <span slot="title">通知公告</span>
+		      </el-menu-item>
+		      <el-menu-item index="Paper" >
+		        <i class="el-icon-document"></i>
+		        <span slot="title">学术论文</span>
+		      </el-menu-item>
+		      <el-menu-item index="Reform">
+		        <i class="el-icon-setting"></i>
+		        <span slot="title">改革成果项目</span>
+		      </el-menu-item>
+		    </el-menu>
+	</div>
+</template>
+<script>
+  export default {
+	  name:'Sidebar',
+    methods: {
+      handleOpen(key, keyPath) {
+        console.log(key, keyPath);
+      },
+      handleClose(key, keyPath) {
+        console.log(key, keyPath);
+      }
+    }
+  }
+</script>
+<style scoped>
+	
+</style>
---
+++ front-end/innovation/src/views/Backstage/Recommend.vue
@@ -0,0 +1,43 @@
+<template>
+	<div class="maincontainer">
+		<!-- <el-container>
+		  <el-aside style="height: 50vh;" width="200px" ><Sidebar></Sidebar></el-aside>
+		  <el-main>Main</el-main>
+		</el-container> -->
+		<el-row :gutter="20"  class="el-row">
+		  <el-col :span="4"><Sidebar></Sidebar></el-col>
+		  <el-col :span="20">
+			  项目推荐
+		  </el-col>
+		</el-row>
+	</div>
+</template>
+
+<script>
+	import Sidebar from '../../components/Sidebar.vue'
+	export default{
+		components:{
+			Sidebar,
+		},
+		
+	}
+</script>
+
+<style scoped>
+	.el-row {
+	    margin-bottom: 20px;
+		height: 500px;
+	    &:last-child {
+	      margin-bottom: 0;
+	    }
+	  }
+	.maincontainer{
+		height: 100%;
+	}
+	.leftside{
+		
+	}
+	.rightside{
+		
+	}
+</style>
