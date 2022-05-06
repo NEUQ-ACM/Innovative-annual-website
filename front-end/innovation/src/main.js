88fd01492825c104f64768c250d9e199b5f5ab94
十音 <908296267@qq.com>
2022-05-06 15:04:32 +0800

    news notice后台

--- front-end/innovation/src/main.js
+++ front-end/innovation/src/main.js
@@ -5,11 +5,10 @@ import store from './store'
 import ElementUI from 'element-ui'
 import axios from 'axios'
 import 'element-ui/lib/theme-chalk/index.css'
-Vue.prototype.$axios=axios
+Vue.prototype.$axios = axios
 Vue.config.productionTip = false
 Vue.use(ElementUI)
 
-axios.defaults.baseURL = 'http://81.70.56.45:8082'
 
 new Vue({
   router,
@@ -17,3 +16,5 @@ new Vue({
   axios,
   render: h => h(App)
 }).$mount('#app')
+
+axios.defaults.baseURL = 'http://81.70.56.45:8082'
