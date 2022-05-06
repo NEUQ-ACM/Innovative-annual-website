8a925bf96e05ee3520604bfb381e0e8c97589c32
lyntele <87609128+lyntele@users.noreply.github.com>
2022-05-06 09:59:16 +0800

    完成部分作品展示功能

---
+++ front-end/innovation/src/views/ItemDetail.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <!-- 面包屑导航区域 -->
+        <el-breadcrumb separator="/" class="breadcrumb">
+          <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+          <el-breadcrumb-item>作品展示</el-breadcrumb-item>
+        </el-breadcrumb>
+        <el-divider></el-divider>
+      </el-col>
+    </el-row>
+    <!-- 作品标题 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <div class="titleBox">
+          <span class="itemTitle">{{ project.project_name }}</span>
+        </div>
+      </el-col>
+    </el-row>
+    <el-col :span="14" :offset="5">
+      <el-divider></el-divider>
+    </el-col>
+    <!-- 校徽 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <el-avatar shape="circle" :size="150" :fit="fit" :src="url"></el-avatar>
+      </el-col>
+    </el-row>
+
+
+  <!-- 作品详情 -->
+    <!-- 基本信息 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <el-descriptions
+          direction="vertical"
+          :column="4"
+          border
+        >
+          <el-descriptions-item label="论文题目"
+            >{{project.project_name}}</el-descriptions-item
+          >
+          <el-descriptions-item label="项目类型"
+            >{{project.project_type}}</el-descriptions-item
+          >
+          <el-descriptions-item label="学科类别" :span="2"
+            >{{project.category}}</el-descriptions-item
+          >
+          <el-descriptions-item label="项目编号">
+            <el-tag size="small">{{project.projectId}}</el-tag>
+          </el-descriptions-item>
+          <el-descriptions-item label="学校">{{project.school}}</el-descriptions-item>
+        </el-descriptions>
+      </el-col>
+    </el-row>
+    <!-- 参赛人员 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <div class="tableTitleBox">
+          <span class="itemTitle">项目成员</span>
+        </div>
+        <el-table :data="project.studentList" border fit>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="grade" label="年级" width="180">
+          </el-table-column>
+          <el-table-column prop="specialty" label="专业" width="345">
+          </el-table-column>
+          <el-table-column prop="isPresenter" label="是否主持人" width="180">
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+    <!-- 指导老师 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+        <div class="tableTitleBox">
+          <span class="itemTitle">指导老师</span>
+        </div>
+        <el-table :data="project.teacherList" border fit>
+          <el-table-column prop="name" label="姓名" width="180">
+          </el-table-column>
+          <el-table-column prop="grade" label="职称" width="180">
+          </el-table-column>
+          <el-table-column prop="specialty" label="研究方向" width="345">
+          </el-table-column>
+        </el-table>
+      </el-col>
+    </el-row>
+    <!-- 论文正文 -->
+    <el-row>
+      <el-col :span="14" :offset="5">
+       <el-card shadow="never">
+         {{project.description}}
+       </el-card>
+      </el-col>
+    </el-row>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "",
+  data() {
+    return {
+      project: {
+        id: "",
+        projectId: "",
+        project_name: "test1",
+        description: "test1",
+        project_type: "",
+        category: "",
+        type: "",
+        view_counts: "",
+        previewImageUrl: "",
+        badgeUrl: " ",
+        years: "",
+        school: "",
+        studentList: [
+          {
+            id: "321",
+            name: "张三",
+            grade: "22",
+            specialty: "1",
+            isPresenter: "123",
+          },
+          {
+            id: "431",
+            name: "李四",
+            grade: "66",
+            specialty: "34",
+            isPresenter: "424",
+          },
+        ],
+        teacherList: [
+          {
+            id: "234",
+            name: "王五",
+            grade: "22",
+            project_type: "",
+          },
+          {
+            id: "523",
+            name: "赵大",
+            grade: "67",
+            specialty: "31",
+          },
+        ],
+      },
+    };
+  },
+  methods: {},
+  mounted() {
+    console.log(this.$route.query.itemId);
+  },
+};
+</script>
+
+<style scoped>
+.el-breadcrumb {
+  margin-top: 10px;
+}
+.itemTitle {
+  font-size: 20px;
+}
+.titleBox {
+  /* background-color: aqua; */
+  text-align: center;
+  margin-top: 10px;
+}
+.tableTitleBox {
+  /* background-color: aqua; */
+  text-align: left;
+  margin-top: 20px;
+}
+
+.el-table {
+  margin-top: 10px;
+}
+
+.el-card {
+  margin-top: 20px;
+}
+</style>
