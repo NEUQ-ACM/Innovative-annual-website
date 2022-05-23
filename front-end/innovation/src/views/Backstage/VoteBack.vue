dc0cc00ce75835570696620600c6ff8916c5ed18
Scream <76878170+BreettySun@users.noreply.github.com>
2022-05-23 22:24:35 +0800

    用户最大投票数

--- front-end/innovation/src/views/Backstage/VoteBack.vue
+++ front-end/innovation/src/views/Backstage/VoteBack.vue
@@ -5,6 +5,16 @@
       <el-col :span="4">
         <Sidebar></Sidebar>
       </el-col>
+      <el-col :span="10"
+              class="input">
+        <div style="width:200px;margin-top:10px">用户最大投票数：</div>
+        <el-input v-model="input"
+                  :placeholder="'当前投票数'"></el-input>
+        <el-button type="primary"
+                   size="mini"
+                   style="margin-left:1%"
+                   @click="updateUserVoteNum()">提交</el-button>
+      </el-col>
       <el-col :span="19"
               class="table">
         <el-table :data="voteDataRecommend"
@@ -47,10 +57,14 @@ export default {
   },
   data () {
     return {
+      input: 10,
       current: 1,
       total: 0,
       tableDataRecommend: [],
       voteDataRecommend: [],
+      headers: {
+        token: "b196a83e-2e3e-4d04-be19-6bc5aceb0caf"
+      }
     }
   },
   methods: {
@@ -95,14 +109,43 @@ export default {
           this.getVoteNum()
         })
     },
+    getUserVoteNum () {
+      this.$axios
+        .get("http://81.70.56.45:8083/user/getVoteNum", {
+          headers: {
+            "token": this.headers.token
+          }
+        })
+        .then((res) => {
+          console.log(res);
+          this.input = res.data.data
+        })
+    },
+    updateUserVoteNum () {
+      this.$axios
+        .get("http://81.70.56.45:8083/user/updateVoteNum?voteNum=" + this.input, {
+          headers: {
+            "token": this.headers.token
+          }
+        })
+        .then((res) => {
+          console.log(res);
+          this.$message.success('更新成功')
+        })
+    }
   },
   mounted () {
     this.getData()
+    this.getUserVoteNum()
   }
 }
 </script>
 
 <style scoped>
+.input {
+  margin-top: 10px;
+  display: flex;
+}
 .table {
   margin-top: 20px;
 }
