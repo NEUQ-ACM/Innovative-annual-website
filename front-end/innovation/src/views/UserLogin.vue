cdbe3c6060737a9c181f44e19c4c188a14c38511
Scream <76878170+BreettySun@users.noreply.github.com>
2022-05-26 04:08:23 +0800

    Add files via upload

--- front-end/innovation/src/views/UserLogin.vue
+++ front-end/innovation/src/views/UserLogin.vue
@@ -1,37 +1,24 @@
 <template>
   <div>
     <div class="login_container">
-    <!-- 登录盒子 -->
-    <div class="login_box">
-      <h2>用户登录</h2>
-      <!-- 登录表单 -->
-      <el-form
-        :model="loginForm"
-        :rules="rules"
-        ref="loginFormRef"
-        class="login_form"
-      >
-        <el-form-item prop="username">
-          <el-input
-            v-model="loginForm.username"
-            prefix-icon="el-icon-user"
-            placeholder="请输入用户名"
-          ></el-input>
-        </el-form-item>
-        <el-form-item prop="password">
-          <el-input
-            v-model="loginForm.password"
-            prefix-icon="el-icon-lock"
-            placeholder="请输入密码"
-          ></el-input>
-        </el-form-item>
-        <el-form-item class="btns">
-          <el-button type="primary" @click="login">登录</el-button>
-          <el-button type="info" @click="resetLoginForm">重置</el-button>
-        </el-form-item>
-      </el-form>
+      <!-- 登录盒子 -->
+      <div class="login_box">
+        <h2>用户登录</h2>
+        <!-- 登录表单 -->
+        <el-form :model="loginForm" :rules="rules" ref="loginFormRef" class="login_form">
+          <el-form-item prop="username">
+            <el-input v-model="loginForm.username" prefix-icon="el-icon-user" placeholder="请输入用户名"></el-input>
+          </el-form-item>
+          <el-form-item prop="password">
+            <el-input v-model="loginForm.password" prefix-icon="el-icon-lock" placeholder="请输入密码"></el-input>
+          </el-form-item>
+          <el-form-item class="btns">
+            <el-button type="primary" @click="login">登录</el-button>
+            <el-button type="info" @click="resetLoginForm">重置</el-button>
+          </el-form-item>
+        </el-form>
+      </div>
     </div>
-  </div>
   </div>
 </template>
 
@@ -41,8 +28,9 @@ export default {
   data() {
     return {
       loginForm: {
-        username: 'admin',
-        password: 'admin',
+        username: '',
+        password: '',
+        status: 0,
       },
       // 表单校验规则
       rules: {
@@ -59,14 +47,37 @@ export default {
     // 登录
     async login() {
       console.log('函数调用')
-      const {data: res} = await this.$axios.post('http://81.70.56.45:8083/user/login?satoken=2210ee6c-f98d-43f2-92ee-a286f82c7c6f', this.loginForm)
-      if(res.status !== 200) {
-        return this.$message.error(res.msg)
+      const _this = this
+
+      const { data: res } = await _this.$axios.post('http://81.70.56.45:8083/user/login?satoken=2210ee6c-f98d-43f2-92ee-a286f82c7c6f', this.loginForm)
+      if (res.status !== 200) {
+        return _this.$message.error(res.msg)
       }
       else {
-        console.log(res)
+        // console.log(res)
         window.sessionStorage.setItem('token', res.data)
-        this.$router.push('/vote')
+
+        /**
+         * -向后端获取投票功能的状态
+         *    -1，可以投票，跳转
+         *    -2，投票已关闭
+         *    -3，投票还没开始
+         */
+        _this.$axios.get('http://81.70.56.45:8083/voteState/get').then(res => {
+          console.log(res)
+          _this.status = res.data.data.status
+          if (_this.status == '已开启') {
+            this.$router.push('/vote')
+          } else if (_this.status == '未开始') {
+            _this.$message.error("投票通道还未开启")
+          } else if (_this.status == '已结束') {
+            _this.$message.error("投票已结束")
+          }
+        })
+
+
+
+
       }
     },
     // 重置登录表单
@@ -74,7 +85,7 @@ export default {
       this.$refs.loginFormRef.resetFields()
     },
   },
-  mounted() {},
+  mounted() { },
 };
 </script>
 
@@ -84,6 +95,7 @@ export default {
   height: 100%;
   background-size: 100% 100%;
 }
+
 .login_box {
   width: 450px;
   height: 300px;
